2085f6ec7c3c8059964cbc0a2db2dcab84300c03
yh <351749498@qq.com>
2017-09-06 20:13:26 +0800

    注释语句

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+  
+      <leftNav></leftNav>
+      <Pos></Pos>
+      <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import leftNav from './components/leftNav'
+import Pos from './components/pos'
+export default {
+  name: 'app',
+  components : {
+    leftNav,Pos
+  },
+  created(){
+    let oHeight=document.body.clientHeight;
+    document.getElementById("app").style.height= oHeight + "px";
+
+  }
+
+
+}
+</script>
+
+<style>
+
+*{ margin:0; padding:0; }
+
+html,body,#app{ height:100%;
+           background:#f1f1f1; }
+
+li{ list-style:none; }
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+}
+</style>
---
+++ src/components/leftNav.vue
@@ -0,0 +1,49 @@
+<template>
+	<div class="leftNav" id="nav">
+		<ul>
+			<li>
+				<i class="icon iconfont icon-dianpu"></i>
+				<em>店铺</em>
+			</li>
+			<li>
+				<i class="icon iconfont icon-shouyin"></i>
+				<em>外卖</em>
+			</li>
+			<li>
+				<i class="icon iconfont icon-gerenzhongxinxia"></i>
+				<em>设置</em>
+			</li>
+
+
+		</ul>
+
+	</div>
+</template>
+
+<script>
+export default{
+	name : "leftNav"
+}
+
+</script>
+
+<style>
+
+.leftNav{ width:5%;
+		  height:100%;
+		  background:#004FAC; 
+		  float:left;}
+
+
+.icon{ color:#fff ;
+	    font-size:20px;}
+
+.leftNav li{ padding:15px 0;
+			 cursor:pointer;  }
+.leftNav li em{ font-style:normal; 
+				font-size:20px;
+				display:block;
+				color:#fff;
+				margin: 5px 0}
+
+</style>
